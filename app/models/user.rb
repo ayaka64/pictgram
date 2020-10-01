b5b5115fbf4954ea552d5d0a80c52a9ee7aaf452
ayaka <asmnkys@gmail.com>
2020-10-01 20:21:47 +0000

    add new file

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,13 @@
 
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 15 }
-  validates :email, presence: true,
+  validates :email, presence: true
   
   has_secure_password
+  
+  validates :password, presence: true, length: { minimum: 8, maximum: 32 }, 
+  format: {with: VALID_PASSWRD_REGEX }
+    
+  VALID_PASSWRD_REGEX =/\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
   
 end
