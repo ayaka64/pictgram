8cbead1df3dda12aa64f940b928285b634e8b3ef
ayaka <asmnkys@gmail.com>
2020-11-06 13:46:46 +0000

    Rails07 kadai

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,12 +3,13 @@ class FavoritesController < ApplicationController
     @favorite_topics = current_user.favorite_topics
   end
   
-  binding.pry
   
   def create
     favorite = Favorite.new
     favorite.user_id = current_user.id
     favorite.topic_id = params[:topic_id]
+    
+    #binding.pry
     
     if favorite.save
       redirect_to topics_path, success: 'お気に入りに登録しました'
@@ -16,6 +17,12 @@ class FavoritesController < ApplicationController
     else
       redirect_to topics_path, danger: 'お気に入り登録に失敗しました'
     end
+  end
+  
+  def destroy
+    @favorite = Favorite.find_by(user_id: current_user.id, topic_id: params[:topic_id])
+    @favorite.destroy
+    redirect_to topics_path, success: 'お気に入りを取り消しました'
   end
 end
 
